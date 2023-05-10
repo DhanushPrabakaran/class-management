928295361232c233476c0eb3453913b50a826a7c
samR343 <samramesh343@gmail.com>
2023-05-10 09:15:46 +0530

    changed

---
+++ verify.js
@@ -0,0 +1,54 @@
+constform = document.getElementById('form')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+})
+constform = document.getElementById('form')
+
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+const youremail = document.getElementById('email').value 
+const yourpassword = document.getElementById('password').value 
+const yourfirstname= document.getElementById('fn').value 
+const yoursecondname= document.getElementById('sn').value 
+const yourmale = document.getElementById('m').value 
+const yourfemale = document.getElementById('f').value 
+const yourothers= document.getElementById('o').value 
+const yourfather = document.getElementById('fn').value 
+const yourmother = document.getElementById('mn').value 
+const yourschool=document.getElementById('sd')
+const yourmark=document.getElementById('ma')
+const yourcollege=document.getElementById('cd')
+const yourpercentage=document.getElementById('cp')
+const yourachivement=document.getElementById('ac')
+const yourhobbies=document.getElementById('ho')
+const yourskill=document.getElementById('sk')
+const yourmobile=document.getElementById('c')
+const yourwatsapp=document.getElementById('c')
+const yourrank=document.getElementById('c')
+const yourchief=document.getElementById('c')
+const yourimage=document.getElementById('file')
+
+console.log(youremail)
+console.log(yourpassword)
+console.log(yourfirstname)
+console.log(yoursecondname)
+console.log(yourmale)
+console.log(yourfemale)
+console.log(yourothers)
+console.log(yourfather)
+console.log(yourmother)
+console.log(yourschool)
+console.log(yourmark)
+console.log(yourcollege)
+console.log(yourpercentage)
+console.log(yourachivement)
+console.log(yourhobbies)
+console.log(yourskill)
+console.log(yourmobile)
+console.log(yourwatsapp)
+console.log(yourrank)
+console.log(yourchief)
+console.log(yourimage)
+
+})
